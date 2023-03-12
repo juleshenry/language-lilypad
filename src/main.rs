9cdff6d275d4fee7e28c89816fafa70eed0c8855
juleshenry <julianplushenry@gmail.com>
2023-03-12 18:20:20 -0500

    Working updating example to input

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,23 @@ use wasm_bindgen::prelude::wasm_bindgen;
 // Then, ingest user input here for the desired reactivity to WASM effect!
 #[wasm_bindgen(module = "/js/index.js")]
 extern "C" {
-    fn multiply(a: f64, b: f64) -> f64;
+    fn multiply(a: String) -> String;
 }
 
 #[component]
 fn App<G: Html>(cx: Scope) -> View<G> {
-    let a = create_signal(cx, "0");
-    let b = create_signal(cx, "0");
-    let product = create_signal(cx, 0_f64);
+    let a = create_signal(cx, String::new());
+    // // let b = create_signal(cx, String::from("0").as_str());
+    let product = create_signal(cx, String::new());
 
     create_effect(cx, || {
-        product.set(multiply(
-            *(a.get().parse::<f64>().unwrap()),
-            *(a.get().parse::<f64>().unwrap()),
-        ));
+        product.set(a.get().to_string());
     });
 
     view! { cx,
-        input(type="number", bind:value=a)
-        span { "*" }
-        input(type="number", bind:value=b)
+        input(type="text", bind:value=a)
+        span { "times" }
+        // input(type="number", bind:value=b)
         span { "=" }
         span { (*product.get()) }
     }
