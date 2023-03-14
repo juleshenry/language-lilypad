ae4689ac9e3cf6cc065ac48e077642858385e811
juleshenry <julianplushenry@gmail.com>
2023-03-14 15:28:28 -0500

    simplest sycamore

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,31 @@
 use sycamore::prelude::*;
 use wasm_bindgen::prelude::wasm_bindgen;
-//NOTE to self... there is bug in the Sycamore example.
-// It would behoove you to create a pull request
-// Deeply, for this project we can munge index.js to perform the API call
-// Then, ingest user input here for the desired reactivity to WASM effect!
+// use wasm_bindgen::JsValue;
+// use std::future::Future;
+
 #[wasm_bindgen(module = "/js/index.js")]
 extern "C" {
-    fn multiply(a: String) -> String;
+    fn fetch_def(a: String) -> String;
 }
 
-fn add_exclamation(mut blah: String) -> String {
-    blah.push_str(String::from("!").as_str());
-    return blah
-}
+
+// fn convert_future_to_string(fut: String) -> String {
+//     return String::from("Hello, world!");
+// }
 
 #[component]
 fn App<G: Html>(cx: Scope) -> View<G> {
     let a = create_signal(cx, String::new());
-    // // let b = create_signal(cx, String::from("0").as_str());
     let product = create_signal(cx, String::new());
 
     create_effect(cx, || {
-        let mut user_input = a.get().to_string();
-        user_input = multiply(user_input);
-        let b = add_exclamation(user_input);
-        product.set(b);
+        let user_input = a.get().to_string();
+        product.set(fetch_def(user_input));
     });
 
     view! { cx,
         input(type="text", bind:value=a)
-        span { "times" }
-        // input(type="number", bind:value=b)
-        span { "=" }
-        span { (*product.get()) }
+        div { (*product.get()) }
     }
 }
 
