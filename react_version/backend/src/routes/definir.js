b84c3f629fcccff74ee501422b2a340aa040dc98
Julian Henry <julianplushenry@gmail.com>
2023-11-18 17:04:16 -0600

    GET MONEY!

--- react_version/backend/src/routes/definir.js
+++ react_version/backend/src/routes/definir.js
@@ -2,23 +2,118 @@
 
 const express = require('express');
 const router = express.Router();
+const {
+    Sequelize,
+    DataTypes
+} = require('sequelize');
+const path = require('path');
 
-function definir(palabra) {
-  // Your logic to define the word goes here
-  const definicion = 'DEFINED';
-  return { definicion };
+const sequelize = new Sequelize({
+dialect: 'sqlite',
+storage: path.join(__dirname, '../../db/palabras.db')
+});
+
+// Define a model for your entries
+const Entry = sequelize.define('palabras', {
+palabra: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+},
+definicion: Sequelize.TEXT,
+}, {
+timestamps: false,
+});
+  
+sequelize.sync();
+
+// async function definir(palabra) {
+//     console.log('pegar');
+//     try {
+//         const entries = await Entry.findOne({
+//             where: {
+//                 palabra: palabra
+//             }
+//         });
+//         return entries;
+//     } catch (error) {
+//         throw new Error('Error fetching entries');
+//     }
+// }
+
+async function definir(palabra) {
+    console.log('pegar');
+    return new Promise(async (resolve, reject) => {
+        try {
+            const entries = await Entry.findOne({
+                where: {
+                    palabra: palabra
+                }
+            });
+            // return entries;
+            resolve(entries);
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
+  
+function syncDef(palabra) {
+// Returning a promise from syncFunc
+    return new Promise(async (resolve, reject) => {
+        try {
+            // Calling asyncFunc using await
+            const result = await definir(palabra);
+            resolve(result);
+        } catch (error) {
+            reject(error);
+        }
+});
+}
+
+// An asynchronous function that queries the database
+async function asyncFunc() {
+    try {
+      // Simulating a Sequelize query to find a user
+      const user = await Entry.findOne({ where: { palabra: 'loción' } });
+      return user; // Returning the result of the query
+    } catch (error) {
+      throw error; // Propagating any errors that occurred during the query
+    }
+  }
+  
+  // Synchronous function that calls asyncFunc
+  function syncFunc() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await asyncFunc();
+        resolve(result);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+  
+
+
 
 // Define the route for '/definir'
 router.get('/', (req, res) => {
   const palabra = req.body.palabra;
-
   if (!palabra) {
     return res.status(400).json({ error: 'Palabra not provided in the request body' });
   }
+  syncFunc(palabra).then((result) => {
+    console.log('?')
+    console.log(result); // Output: Async operation complete
+    const definicion = 'exito';
+    res.json(result);
+  })
+  .catch((error) => {
+    console.error(error.message);
+    const definicion = 'fail';
+    res.json(definicion);
+  });
 
-  const resultado = definir(palabra);
-  res.json(resultado);
 });
 
 module.exports = router;
