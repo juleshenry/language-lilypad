f6f03bf5c203c2cc8c29b65a29c3b97d477ef497
Julian Henry <julianplushenry@gmail.com>
2023-11-22 18:18:31 -0600

    We all self-conscious. I'm just the first to admit it.

--- react_version/frontend/ll/components/ApiCall.js
+++ react_version/frontend/ll/components/ApiCall.js
@@ -1,49 +1,50 @@
-  import React, { useState } from 'react';
-  import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-  const ApiCall = () => {
-    const [inputValue, setInputValue] = useState('');
-    const [result, setResult] = useState('');
+const ApiCall = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("");
 
-    const handleInputChange = (e) => {
-      setInputValue(e.target.value);
-    };
-
-    const handleApiCall = async () => {
-      try {
-        console.log(`called ${inputValue}`);
-      //   const response = await axios.get(`http://localhost:3001/definir?palabra=${inputValue}`);
-      const response = await axios.get(`http://localhost:3001/definir?palabra=${inputValue}`,
-      {
-          // method: 'GET',
-          // mode: 'cors',
-          // cache: 'no-cache',
-          // credentials: 'same-origin' ,
-          headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-          },
-        });
-
-      //   response = await axios.get(`https://api.kanye.rest/`);
-        setResult(response.json()); // Adjust this based on your API response structure
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setResult('Error fetching data');
-      }
-    };
-
-    return (
-      <div>
-        <div style={{ textAlign: 'center', margin: '20px' }}>
-          <input type="text" value={inputValue} onChange={handleInputChange} />
-          <button onClick={handleApiCall}>Call API</button>
-        </div>
-        <div style={{ textAlign: 'center' }}>
-          <p>API Result:</p>
-          <p>{result?.quote || result?.definicion}</p>
-        </div>
-      </div>
-    );
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
   };
 
-  export default ApiCall;
+  const handleApiCall = async () => {
+    try {
+      console.log(`we called ${inputValue}`);
+      //   const response = await axios.get(`http://localhost:3001/definir?palabra=${inputValue}`);
+      // curl -X GET -H "Content-Type: application/json" -d '{"palabra": "sin"}' http://localhost:3031/definir
+
+        // `http://localhost:3031/definir?palabra=${inputValue}`,
+      const response = await axios.get(
+        `https://api.kanye.rest/`,
+        // {palabra : inputValue},
+        // {
+        //   headers: {
+        //     "Content-Type": "application/json;charset=utf-8",
+        //   },
+        // },
+      );
+      console.log(response);
+      setResult(response.data); // Adjust this based on your API response structure
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setResult("Error fetching data");
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ textAlign: "center", margin: "20px" }}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleApiCall}>Call API</button>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <p>API Result:</p>
+        <p>{result.quote}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ApiCall;
