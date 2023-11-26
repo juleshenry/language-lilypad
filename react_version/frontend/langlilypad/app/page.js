56c5cfb550b7b8a42b644180abaa1cd2f5d7089c
Julian Henry <julianplushenry@gmail.com>
2023-11-26 16:48:50 -0600

    KOBEYASHI

--- react_version/frontend/langlilypad/app/page.js
+++ react_version/frontend/langlilypad/app/page.js
@@ -1,106 +1,99 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
   const [response, setResponse] = useState(null);
-  
   const [inputValue, setInputValue] = useState("");
 
+  const limpia = (e) => {
+    return e
+      .replaceAll("\n", " ")
+      .replaceAll(",", "")
+      .replaceAll(".", "")
+      .replaceAll("-", "")
+      .replaceAll("-", "")
+      .replaceAll("'", "")
+      .replaceAll("(", "").toLowerCase();
+  };
+
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+    setInputValue(limpia(e.target.value));
   };
 
   useEffect(() => {
-    // This code runs after the component has rendered and inputValue has been updated
-    console.log('Input has become... ', inputValue,' ...!');
     definirData();
-  }, [inputValue]); // The effect will run whenever inputValue changes
+  }, [inputValue]);
 
   const handleInputClick = async (word) => {
-    console.log('Setting...', word,'... to inputValue');
-    setInputValue(word);
-    // console.log('Input has become... ', inputValue,' ...!');
-    
-  };
-
-  // fetch hard-coded BE call
-  const fetchData = async () => {
-    try {
-      console.log('fetch BE directly from button');
-      let d = {'palabra' : 'sin'};
-      const res = await fetch('http://localhost:3000/definir',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(d), 
-      }
-      );
-      // const res = await fetch('http://localhost:3000/definir?palabra=sin');
-      const data = await res.json();
-      setResponse(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+    setInputValue(limpia(word));
   };
 
   // fetch dynamic BE calls
   const definirData = async () => {
     try {
-      console.log('fetching ' + inputValue + ' from backend');
-      let d = {'palabra' : inputValue};
-      const res = await fetch('http://localhost:3000/definir',
-      {
-        method: 'POST',
+      console.log("fetching " + limpia(inputValue) + " from backend");
+      let d = { palabra: limpia(inputValue) };
+      const res = await fetch("http://localhost:3000/definir", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(d), 
-      }
-      );
+        body: JSON.stringify(d),
+      });
       const data = await res.json();
       setResponse(data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
   const makeWordsClickable = () => {
-    const words = response?.definicion?.split(' ');
-    if (! words) {
-      return (<span>Nada.</span>);
+    const words = response?.definicion?.split(" ");
+    if (!words) {
+      return <span>Nada.</span>;
     }
+    // console.log( new Map(Object.entries(response?.palabras)).keys() );
     return words.map((word, index) => (
       <span
         key={index}
-        className='clickable-word'
-        onClick={async () => {await handleInputClick(word);}}
+        className={
+          word in response?.palabras ? "clickable-word" : "unclickable-word"
+        }
+        onClick={async () => {
+          await handleInputClick(word);
+        }}
       >
-        {word + (index === words.length - 1 ? '' : ' ')}
+        {word + (index === words.length - 1 ? "" : " ")}
       </span>
     ));
   };
 
   return (
     <div>
-        <div style={{ textAlign: "center", margin: "20px","font-size": "69px" }}>
-          <h1>Language Lilypad</h1>
-          <h1>🪷🌺🐸🌺🪷</h1>
-          <input style={{textAlign: "center", height: "50px", color: "#5EDD5F", "font-size": "40px"}} type="text" value={inputValue} onChange={handleInputChange} />
-        </div>
-        <div style={{ textAlign: "center" }}>
-          <p>
-            {makeWordsClickable(JSON.stringify(response?.definicion, null, 2))}
-          </p>
-          {/* <p>API Result:</p> */}
-          {/* <p>{JSON.stringify(response, null, 2)}</p> */}
-        </div>  
+      <div style={{ textAlign: "center", margin: "20px", "fontSize": "69px" }}>
+        <h1>Language Lilypad</h1>
+        <h1>🪷🌺🐸🌺🪷</h1>
+        <input
+          style={{
+            textAlign: "center",
+            height: "50px",
+            color: "#5EDD5F",
+            "fontSize": "40px",
+          }}
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <p>
+          {makeWordsClickable(JSON.stringify(response?.definicion, null, 2))}
+        </p>
+        {/* <p>API Result:</p> */}
+        {/* <p>{JSON.stringify(response, null, 2)}</p> */}
+      </div>
     </div>
   );
 };
 
-
-
 export default Home;
-
